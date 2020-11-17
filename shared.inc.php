99a40ee223d8f685389497d684e6f16cbd40f2cb
Johnathan <74081230+johnlee111@users.noreply.github.com>
2020-11-16 18:32:26 -0600

    Add files via upload

--- shared.inc.php
+++ shared.inc.php
@@ -1,9 +1,116 @@
 <?php
 
-$header = "<header>\n
-			<h1>A Fake Site</h1>\n
-			<h3>For CTEC 4309 PHP Exercise Only</h3>\n
-		</header>\n";
 
+$nav = "
+			<nav>
+				<a class='navbar-brand' href='index.php'>Brand</a> <button class='navbar-toggler' data-target='#mainNavigation' type='button'><i class='far fa-bars'></i></button>
+				<div class='collapse navbar-collapse' id='mainNavigation'>
+					<ul class='navbar-nav'>
+						<li class='nav-item'>
+							<a class='nav-link' href='index.php'>Home</a>
+						</li>
+						<li class='nav-item'>
+							<a class='nav-link' href='aboutus.html'>About Us</a>
+						</li>
+						<li class='nav-item'>
+							<a class='nav-link' href='repair.html'>Repair Services</a>
+						</li>
+						<li class='nav-item'>
+							<a class='nav-link' href='contact.php'>Contact Us</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		\n";
 
+$weekday = '&nbsp;&nbsp;&nbsp;9   a.m.  &nbsp;&nbsp;&nbsp; -  &nbsp;&nbsp;&nbsp; 7  p.m.';
+$weekend = '&nbsp;&nbsp;&nbsp;12  p.m.  &nbsp;&nbsp;&nbsp; -   &nbsp;&nbsp;&nbsp;7  p.m.';
+
+ $footer = "<footer class='footer-distributed'>
+	 <div class='desktop'>
+		 <div class='footer-one'><img alt='fastphonerepairlogo' src='images/logo1.png'></div>
+		 <div class='footer-two'>
+			 <h5>Contact</h5>
+			 <div>
+				 <h6><i class='fas fa-map-marker-alt'></i>2218 S Bowen Rd, Arlington, TX 76013</h6>
+				 <h6><i class='fas fa-phone-alt'></i>682 – 553 – 8787</h6>
+				 <h6><i class='fas fa-envelope'></i> <a href='mailto:fastphonerepair2218@gmail.com'>fastphonerepair2218@gmail.com</a></h6>
+			 </div>
+		 </div>
+
+		 <div class='footer-three'>
+			 <h5>Navigation</h5>
+			 <div class='footer-nav'>
+				 <h6><a href='index.php'>Home</a></h6>
+				 <h6><a href='aboutus.html'>About Us</a></h6>
+				 <h6><a href='repair.html'>Repair Services</a></h6>
+				 <h6><a href='contact.php'>Contact Us</a></h6>
+			 </div>
+		 </div>
+
+		 <div class='footer-four'>
+			 <div class='footer-icons'>
+				 <ul class='social-media'>
+					 <li class='text'>
+						 <h5>Follow Us</h5>
+					 </li>
+					 <li>
+						 <a href='https://www.facebook.com/PhoneRepairArlington/' title='Facebook'><i class='fab fa-facebook-square'></i></a>
+					 </li>
+					 <li>
+						 <a href='https://twitter.com/asiankingbuffet' title='Twitter'><i class='fab fa-twitter-square'></i></a>
+					 </li>
+					 <li>
+						 <a href='https://bit.ly/2W2B4uD' title='Google'><i class='fab fa-google-plus-square'></i></a>
+					 </li>
+					 <li>
+						 <a href='https://www.instagram.com/explore/tags/chinesefood/?hl=en' title='Instagram'><i class='fab fa-instagram-square'></i></a>
+					 </li>
+				 </ul>
+			 </div>
+		 </div>
+
+	 </div><img alt='asiankingbuffet logo' class='tablet-mobile' src='images/logo1.png'>
+	 <div class='tablet-mobile footer-mobile'>
+		 <div class='footer-mobile-one'>
+			 <h5>Contact</h5>
+			 <div>
+				 <h6><i class='fas fa-map-marker-alt'></i>2218 S Bowen Rd, Arlington, TX 76013</h6>
+				 <h6><i class='fas fa-phone-alt'></i>682-553–8787</h6>
+				 <h6><i class='fas fa-envelope'></i> <a href='mailto:fastphonerepair2218@gmail.com'>fastphonerepair2218@gmail.com</a></h6>
+			 </div>
+			 <div class='footer-icons'>
+				 <ul class='social-media'>
+					 <li class='text'>
+						 <h5>Follow Us</h5>
+					 </li>
+					 <li>
+						 <a href='https://www.facebook.com/Asiankingbuffet/' title='Facebook'><i class='fab fa-facebook-square'></i></a>
+					 </li>
+					 <li>
+						 <a href='https://twitter.com/asiankingbuffet' title='Twitter'><i class='fab fa-twitter-square'></i></a>
+					 </li>
+					 <li>
+						 <a href='https://bit.ly/2W2B4uD' title='Google'><i class='ab fa-google-plus-square'></i></a>
+					 </li>
+					 <li>
+						 <a href='https://www.instagram.com/explore/tags/chinesefood/?hl=en' title='Instagram'><i class='fab fa-instagram-square'></i></a>
+					 </li>
+				 </ul>
+			 </div>
+		 </div>
+
+		 <div class='footer-mobile-two'>
+			 <h5>Navigation</h5>
+			 <div class='footer-nav'>
+				 <h6><a href='index.html'>Home</a></h6>
+				 <h6><a href='aboutus.html'>About Us</a></h6>
+				 <h6><a href='repair.html'>Repair Services</a></h6>
+				 <h6><a href='contact.html'>Contact Us</a></h6>
+			 </div>
+		 </div>
+	 </div>
+
+	 <p class='footer-company-name'>Privacy Statement | &copy; All rights Reserved 2020 | Terms of Use</p>
+	 </footer>";
 ?>
